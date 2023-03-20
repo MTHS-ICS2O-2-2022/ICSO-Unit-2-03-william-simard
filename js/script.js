3a639d76fefaf0267d2e7bd0a262e2d93a4e2995
william <124389603+william-simard@users.noreply.github.com>
2023-03-20 16:57:12 +0000

    changed all

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("first-name").value
+  const userAge = parseInt(document.getElementById("age-entered").value)
+
+  // output
+  document.getElementById("address").innerHTML =
+    "Your info is: " + firstName + ", age " + userAge + "."
+}
